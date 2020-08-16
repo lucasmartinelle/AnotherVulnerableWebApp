6be1c3cbcf967bfad46ceb4011bbeed644863ead
Jomar <contact@jomar.fr>
2020-08-16 11:16:04 +0200

    Readme & some files update
    Easier to readme and fix small bugs

--- app/init.php
+++ app/init.php
@@ -1,14 +1,14 @@
 <?php
     define('ACTIVE_DEBUG', false); // false to disable debug, true to enable it
-    define('URL', 'https://'.$_SERVER['HTTP_HOST']); // don't put "/" at the end of URL
+    define('URL', 'http://'.$_SERVER['HTTP_HOST']); // don't put "/" at the end of URL
     define('DEFAULT_PAGE', 'login'); // label of default page
-    define('TITLE', 'Vulnerability'); // Global title
-    define('WEBSITE_PATH', '/var/www/html/'); // website path
+    define('TITLE', 'VulnApp'); // Global title
+    define('WEBSITE_PATH', '/var/www/html/AnotherVulnerableWebApp/'); // website path
 
     // make sure to enable PDO drivers
     define('DB_HOST', '127.0.0.1'); // host MYSQL
-    define('DB_USERNAME', 'root'); // username MYSQL
-    define('DB_PASSWORD', ''); // password MYSQL
+    define('DB_USERNAME', 'vulnerability'); // username MYSQL
+    define('DB_PASSWORD', '6xUm%3moNghtQaZ8Q'); // password MYSQL
     define('DB_DATABASE', 'vulnerability'); // Databse MYSQL
 
     // private and site key of grecaptcha v3
@@ -20,6 +20,6 @@
     define('PASSWORD_SENDER', '');
     define('FROM_SENDER', '');
 
-    define('PASSWORD_ADMIN', '');
-    define('IDENTIFIANT_ADMIN', '');
+    define('PASSWORD_ADMIN', 'Administrator');
+    define('IDENTIFIANT_ADMIN', 'password123');
 ?>
